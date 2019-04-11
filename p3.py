bf39a2aa76e9c5026d71b335ea94901128a53101
LiangHanZheng <915625352@qq.com>
2019-04-11 15:48:47 +0800

    all files added

---
+++ p3.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 11 15:36:22 2019
+
+@author: apple
+"""
+
+print('byebye')
---
+++ p5.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Apr 11 15:30:03 2019
+
+@author: apple
+"""
+
+print("Hello World!")
